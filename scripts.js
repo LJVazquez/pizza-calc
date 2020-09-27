0972cff0351ef83303cd1d6308e57bad0626b27c
Ljvazquez <ljvazquez00@gmail.com>
2020-09-27 19:45:42 -0300

    wip

--- scripts.js
+++ scripts.js
@@ -30,56 +30,22 @@ const sici = document.getElementById('sici');
 const ny = document.getElementById('ny');
 const arg = document.getElementById('arg');
 
-function ing(pizza){
-    return pizzaProp[pizza];
+function ing(type){
+    return pizzaProp[type];
 }
-function napoProp(){
+
+function prop(pizza){
     let weight = document.getElementById('dough-weight');
     let hidra = document.getElementById('hidra');
     let salt = document.getElementById('salt');
     let oil = document.getElementById('oil');
-    weight.value = ing('napo')['weight'];
-    hidra.value = ing('napo')['hidra'];
-    salt.value = ing('napo')['salt'];
-    oil.value = ing('napo')['oil'];
+    weight.value = ing(pizza)['weight'];
+    hidra.value = ing(pizza)['hidra'];
+    salt.value = ing(pizza)['salt'];
+    oil.value = ing(pizza)['oil'];
 }
 
-function siciProp(){
-    let weight = document.getElementById('dough-weight');
-    let hidra = document.getElementById('hidra');
-    let salt = document.getElementById('salt');
-    let oil = document.getElementById('oil');
-    weight.value = ing('sici')['weight'];
-    hidra.value = ing('sici')['hidra'];
-    salt.value = ing('sici')['salt'];
-    oil.value = ing('sici')['oil'];
-}
-function nyProp(){
-    let weight = document.getElementById('dough-weight');
-    let hidra = document.getElementById('hidra');
-    let salt = document.getElementById('salt');
-    let oil = document.getElementById('oil');
-    weight.value = ing('ny')['weight'];
-    hidra.value = ing('ny')['hidra'];
-    salt.value = ing('ny')['salt'];
-    oil.value = ing('ny')['oil'];
-}
-
-function argProp(){
-    let weight = document.getElementById('dough-weight');
-    let hidra = document.getElementById('hidra');
-    let salt = document.getElementById('salt');
-    let oil = document.getElementById('oil');
-    weight.value = ing('arg')['weight'];
-    hidra.value = ing('arg')['hidra'];
-    salt.value = ing('arg')['salt'];
-    oil.value = ing('arg')['oil'];
-}
-
-napo.addEventListener('click', napoProp);
-sici.addEventListener('click', siciProp);
-ny.addEventListener('click', nyProp);
-arg.addEventListener('click', argProp);
-
-
-
+napo.addEventListener('click', () => prop('napo'));
+sici.addEventListener('click', () => prop('sici'));
+ny.addEventListener('click', () => prop('ny'));
+arg.addEventListener('click', () => prop('arg'));
