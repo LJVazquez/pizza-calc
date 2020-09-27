17354d001a2f044b6838841668d8d61d1fa4802c
Ljvazquez <ljvazquez00@gmail.com>
2020-09-27 18:12:32 -0300

    added pepper

--- scripts.js
+++ scripts.js
@@ -4,18 +4,21 @@ const pizzaProp = {
         hidra: 65,
         salt: 3,
         oil: 1,
+        pepper: 0,
     },
     sici: {
         weight: 650,
         hidra: 75,
         salt: 2,
         oil: 2,
+        pepper: 0,
     },
     ny: {
         weight: 240,
         hidra: 75,
         salt: 2,
         oil: 3,
+        pepper: 0,
     }
 };
 
@@ -28,30 +31,36 @@ function napoProp(){
     let hidra = document.getElementById('hidra');
     let salt = document.getElementById('salt');
     let oil = document.getElementById('oil');
+    let pepper = document.getElementById('pepper');
     weight.value = pizzaProp['napo']['weight'];
     hidra.value = pizzaProp['napo']['hidra'];
     salt.value = pizzaProp['napo']['salt'];
     oil.value = pizzaProp['napo']['oil'];
+    pepper.value = pizzaProp['napo']['pepper'];
 }
 function siciProp(){
     let weight = document.getElementById('dough-weight');
     let hidra = document.getElementById('hidra');
     let salt = document.getElementById('salt');
     let oil = document.getElementById('oil');
+    let pepper = document.getElementById('pepper');
     weight.value = pizzaProp['sici']['weight'];
     hidra.value = pizzaProp['sici']['hidra'];
     salt.value = pizzaProp['sici']['salt'];
     oil.value = pizzaProp['sici']['oil'];
+    pepper.value = pizzaProp['sici']['pepper'];
 }
 function nyProp(){
     let weight = document.getElementById('dough-weight');
     let hidra = document.getElementById('hidra');
     let salt = document.getElementById('salt');
     let oil = document.getElementById('oil');
+    let pepper = document.getElementById('pepper');
     weight.value = pizzaProp['ny']['weight'];
     hidra.value = pizzaProp['ny']['hidra'];
     salt.value = pizzaProp['ny']['salt'];
     oil.value = pizzaProp['ny']['oil'];
+    pepper.value = pizzaProp['ny']['pepper'];
 }
 
 napo.addEventListener('click', napoProp);
